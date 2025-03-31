bf79a0065209bc5cefa0aadab5056baa0afa0368
King Jaja <150508794+devJaja@users.noreply.github.com>
2025-03-31 18:45:52 +0100

    feat: add cartridge wallet and changes on modal ui (#95)
    * feat: add cartridge wallet and changes on modal ui
    
    * chore:implemented cartridge wallet
    
    * chore:UI update
    
    * removed package.lock file
    
    * chore:build fix
    
    * chore:test fix
    
    ---------
    
    Co-authored-by: yunusabdul <yunusabdulmajidyunus38@gmail.com>

---
+++ frontend/svg/braavos.tsx
@@ -0,0 +1,14 @@
+// src/svg/BraavosIcon.tsx
+import React from 'react';
+
+const BraavosIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 2C8.13 2 5 5.13 5 9v3.5c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V9c0-3.87-3.13-7-7-7zm0 2c2.76 0 5 2.24 5 5v3H7V9c0-2.76 2.24-5 5-5z"
+      fill="#FFFFFF"
+    />
+    <path d="M9 15h6v2H9v-2z" fill="#FFFFFF" />
+  </svg>
+);
+
+export default BraavosIcon;
